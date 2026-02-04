9d3341accd184e4ecf8395676f314b60cb7ef830
namqd201 <namqd201@gmail.com>
2026-02-04 10:36:02 +0700

    cr pro

---
+++ src/main/java/com/nqd/m3s4/M3S4Application.java
@@ -0,0 +1,13 @@
+package com.nqd.m3s4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M3S4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M3S4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nqd/m3s4/M3S4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nqd.m3s4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M3S4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
